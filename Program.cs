b5faf1afc2468a5ab37fa7595c77b07b5766b41b
khalid karam <khalid.tasks@gmail.com>
2024-09-13 01:53:00 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BoxingAndUnBoxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            object obj = 10;
+            DisplayObj("");
+
+        }
+
+        static void DisplayObj(object obj)
+        {
+            string message = (string)obj;
+            Console.WriteLine(message);
+        }
+
+    }
+}
